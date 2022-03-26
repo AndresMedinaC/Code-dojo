30a3acec744a81bcc03f749cf05ad9d68ea479cf
Andrès <wapo.medina.costilla@gmail.com>
2022-03-26 11:00:58 -0600

    iniciando repositorio

---
+++ cosas de java.js/java comentarios.js
@@ -0,0 +1,7 @@
+function name (parameter1, ...) {
+    // code for execution
+}
+
+function myFunction() {
+    alert("mensajito pal pop up");
+}
---
+++ practica 5/eventos.js
@@ -0,0 +1,21 @@
+
+var selecion = document.getElementById ("sel")
+seleccion.onchange =function (){
+    alert(seleccion.value);
+}
+
+
+function abrePopup(){
+    window.open("https://google.com", "google", "width=400, hight=600");
+
+}
+
+
+function ejemplovariablelocal(){
+    var lastname = 'Decuir';
+    console.Log(lastname);
+
+}
+console.Log(lastname);
+
+ejemplovariablelocal();
